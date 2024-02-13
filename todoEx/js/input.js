8d08bdfd858459e72b50114af57eea487d949421
ez0603 <dlwldjs3132@naver.com>
2024-02-13 18:06:28 +0900

    todolist_me01

---
+++ todoEx/js/input.js
@@ -0,0 +1,7 @@
+// const addinputBoxButton = document.querySelector(".input-button");
+
+// addinputBoxButton.onclick = () => {
+//     const todoContentList = document.querySelector(".todo-content-list");
+//     todoContentList[0].innerHTML += 
+
+// }
