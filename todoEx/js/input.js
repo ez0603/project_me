b292ab7eebc2b79c9ba1e93550692ffb68118a23
ez0603 <dlwldjs3132@naver.com>
2024-02-19 20:53:59 +0900

    240219

--- todoEx/js/input.js
+++ todoEx/js/input.js
@@ -3,16 +3,25 @@
 // }
 
 function handleAddclick() {
-    const inputContent = document.querySelector(".input-content")
+    const inputContent = document.querySelector(".input-content");
     const todoContentList = document.querySelector(".todo-content-list");
     console.log(inputContent.value)
-    todoContentList.innerHTML += `
-        <li class="todo-content-box">
-            <div class="todo-content">
-                ${inputContent.value}
-            </div>
-        </li>
-    `
+
+    if(inputContent.value === ""){
+        alert("내용을 입력하시오.")
+    } else {
+        todoContentList.innerHTML += `
+            <li class="todo-content-box">
+                <div class="todo-content">
+                    ${inputContent.value}
+                </div>
+                <div class="todo-content-button-box">
+                    <button>수정</button>
+                    <button>삭제</button>
+                </div>
+            </li>
+        `
+    }
     
         
     inputContent.value = "";
